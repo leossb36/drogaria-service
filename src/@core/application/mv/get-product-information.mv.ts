a7859b20f2ae982a8d7e8fa0a18072d6ced80393
Leonardo Barreiros <leossb36@gmail.com>
2023-03-07 03:24:26 -0300

    fix: fix product dto

--- src/@core/application/mv/get-product-information.mv.ts
+++
@@ -1,8 +0,0 @@
-import { Product } from '@core/infra/integration/model/product.model';
-import { ApiProperty } from '@nestjs/swagger';
-import { BaseModelView } from './base.mv';
-
-export class GetProductInformationModelView extends BaseModelView {
-  @ApiProperty()
-  data: Array<Product>;
-}
