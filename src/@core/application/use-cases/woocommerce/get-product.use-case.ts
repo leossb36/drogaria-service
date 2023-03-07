a2a14fa0c54e42ae1acf0c83ed4f0063f234bcf3
Leonardo Barreiros <leossb36@gmail.com>
2023-03-07 03:17:55 -0300

    fix: fix get products from wp

--- src/@core/application/use-cases/woocommerce/get-product.use-case.ts
+++ src/@core/application/use-cases/woocommerce/get-product.use-case.ts
@@ -1,6 +1,5 @@
 import { getProductWooCommerce } from '@core/application/interface/get-product-woo.interface';
 import { WoocommerceIntegration } from '@core/infra/integration/woocommerce-api.integration';
-import { ValidationHelper } from '@core/utils/validation-helper';
 import { BadRequestException, Injectable } from '@nestjs/common';
 
 @Injectable()
@@ -8,12 +7,12 @@ export class GetProductUseCase {
   constructor(private readonly integration: WoocommerceIntegration) {}
 
   async execute(): Promise<getProductWooCommerce[]> {
-    const { data, status } = await this.integration.getAllProducts();
+    const products = await this.integration.getAllProducts();
 
-    if (!data.length || !ValidationHelper.isOk(status)) {
+    if (!products.length) {
       throw new BadRequestException('Cannot list products!');
     }
 
-    return data;
+    return products;
   }
 }
