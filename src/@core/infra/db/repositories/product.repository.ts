78c18c8f88e8a4874e19760f0c22a225aadf9e0d
Leonardo Barreiros <leossb36@gmail.com>
2023-04-12 10:32:31 -0300

    feat: add product entity

---
+++ src/@core/infra/db/repositories/product.repository.ts
@@ -0,0 +1,55 @@
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product, ProductDocument } from '../schema/product.schema';
+
+@Injectable()
+export class ProductRepository {
+  constructor(
+    @InjectModel(Product.name)
+    private readonly productModel: Model<ProductDocument>,
+  ) {}
+
+  async create(dto: any): Promise<Product> {
+    const product = new this.productModel(dto);
+
+    try {
+      return await product.save();
+    } catch (error) {
+      throw new BadRequestException('Cannot save order on database');
+    }
+  }
+
+  async findAll() {
+    try {
+      const orders = await this.productModel.find({});
+      return orders;
+    } catch (error) {
+      throw new BadRequestException('Cannot find orders on database');
+    }
+  }
+
+  async findById(id: string) {
+    try {
+      const order = await this.productModel.findById(id);
+      return order;
+    } catch (error) {
+      throw new BadRequestException('Cannot find order with this id');
+    }
+  }
+
+  async updateProductBatch(products: any[]) {
+    try {
+      await this.productModel.deleteMany({
+        _id: {
+          $in: [...products.map((product) => product._id)],
+        },
+      });
+
+      const insertMany = await this.productModel.insertMany(products);
+      return insertMany;
+    } catch (error) {
+      throw new BadRequestException('Cannot find order with this id');
+    }
+  }
+}
