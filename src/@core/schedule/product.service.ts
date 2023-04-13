3f9ac7bb23329fcd33aef6517f7ec2f0c23c4e79
Leonardo Barreiros <leossb36@gmail.com>
2023-04-13 17:55:51 -0300

    delete: delete scrap image from schedule

--- src/@core/schedule/product.service.ts
+++ src/@core/schedule/product.service.ts
@@ -1,6 +1,6 @@
 import CustomLogger from '@common/logger/logger';
 import { CreateProductsJsonUseCase } from '@core/application/use-cases/vetor/create-products-json.use-case';
-import { CreateProductUseCase } from '@core/application/use-cases/woocommerce/create-product.use-case';
+// import { CreateProductUseCase } from '@core/application/use-cases/woocommerce/create-product.use-case';
 import { UpdateProductUseCase } from '@core/application/use-cases/woocommerce/update-product.use-case';
 import { Injectable } from '@nestjs/common';
 import { Cron } from '@nestjs/schedule';
@@ -8,21 +8,11 @@ import { Cron } from '@nestjs/schedule';
 @Injectable()
 export class ProductService {
   constructor(
-    private readonly createProductUseCase: CreateProductUseCase,
     private readonly updateProductUseCase: UpdateProductUseCase,
     private readonly createProductsJsonUseCase: CreateProductsJsonUseCase,
   ) {}
 
-  // @Cron('0 0 2 20 * *')
-  async fetchAllProducts() {
-    CustomLogger.info(`[ProductService - fetchAllProductData]  Start job`);
-    const scrapedProducts = this.createProductUseCase.execute();
-    const result = Promise.resolve(await scrapedProducts);
-    CustomLogger.info(`[ProductService - fetchAllProductData]  End job`);
-    return result;
-  }
-
-  // @Cron('0 */5 * * * *')
+  @Cron('0 */5 * * * *')
   async updateProductStock() {
     CustomLogger.info(`[ProductService - updateProductData]  Start job`);
     const products = this.updateProductUseCase.execute();
