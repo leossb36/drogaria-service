e9bd7b29a9b3587010303f2e1d8d8cb050700127
Leonardo Barreiros <leossb36@gmail.com>
2023-04-28 20:14:48 -0300

    feat: create product on wordpress that has no image

---
+++ src/@core/application/use-cases/wordpress/create-image-on-wordpress.use-case.ts
@@ -0,0 +1,23 @@
+import * as mysql from 'mysql2/promise';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class CreateImageOnWordpressUseCase {
+  async addImage(pool: mysql.Pool, productId: any) {
+    const connection = await pool.getConnection();
+    await connection.beginTransaction();
+    try {
+      const notfoundId = 5934;
+      await connection.query(
+        'INSERT INTO wp_postmeta (post_id, meta_key, meta_value) VALUES (?, ?, ?)',
+        [productId, '_thumbnail_id', notfoundId],
+      );
+
+      await connection.commit();
+      return { success: true };
+    } catch (err) {
+      await connection.rollback();
+      return { success: false, message: err.message };
+    }
+  }
+}
